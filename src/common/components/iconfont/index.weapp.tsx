741fa48455003c95cfa6a3f94e42f37b77688cde
墨綠BG <s5460703@gmail.com>
2024-05-16 02:13:00 +0800

    🌈 style：代码格式化

--- src/common/components/iconfont/index.weapp.tsx
+++ src/common/components/iconfont/index.weapp.tsx
@@ -3,8 +3,45 @@
 
 import React, { FunctionComponent } from 'react';
 
-
-export type IconNames = 'wxbpinpaibao' | 'huo' | 'club' | 'fabu' | 'icon' | 'wodeqianbao' | 'wodedingdan' | 'tuiguang' | 'zhaopian' | 'tuiguanghuodong' | 'sousuo' | 'xiaoxi' | 'icon-cutting-edgewe' | 'pinpai' | 'renshu' | 'tiyuyundong' | 'wode' | 'renshu1' | 'shouye' | 'chuqin' | 'dengji' | 'wh-hdjm' | 'huodongjilu' | 'rili' | 'xuexijiaoliu' | 'zongrenshu' | 'index-active' | 'yaoqingruzhu' | 'shangdian' | 'huodongrili' | 'yaoqingruzhu1' | 's-report' | 'a-xinxitixing' | 'shangjiaruzhu' | 'a-bianzu283x' | 'xiaoyouquan' | 'julebu-mian' | 'guanggaohuodongbaogao';
+export type IconNames =
+  | 'wxbpinpaibao'
+  | 'huo'
+  | 'club'
+  | 'fabu'
+  | 'icon'
+  | 'wodeqianbao'
+  | 'wodedingdan'
+  | 'tuiguang'
+  | 'zhaopian'
+  | 'tuiguanghuodong'
+  | 'sousuo'
+  | 'xiaoxi'
+  | 'icon-cutting-edgewe'
+  | 'pinpai'
+  | 'renshu'
+  | 'tiyuyundong'
+  | 'wode'
+  | 'renshu1'
+  | 'shouye'
+  | 'chuqin'
+  | 'dengji'
+  | 'wh-hdjm'
+  | 'huodongjilu'
+  | 'rili'
+  | 'xuexijiaoliu'
+  | 'zongrenshu'
+  | 'index-active'
+  | 'yaoqingruzhu'
+  | 'shangdian'
+  | 'huodongrili'
+  | 'yaoqingruzhu1'
+  | 's-report'
+  | 'a-xinxitixing'
+  | 'shangjiaruzhu'
+  | 'a-bianzu283x'
+  | 'xiaoyouquan'
+  | 'julebu-mian'
+  | 'guanggaohuodongbaogao';
 
 interface Props {
   name: IconNames;
